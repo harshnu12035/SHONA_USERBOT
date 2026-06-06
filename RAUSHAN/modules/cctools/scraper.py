a8df8d0e9830a8c1eca7259840371677b0410e89
harshnu12035 <harshnu12035@gmail.com>
2026-06-06 18:26:39 +0530

    Update scraper.py

--- RAUSHAN/modules/cctools/scraper.py
+++ RAUSHAN/modules/cctools/scraper.py
@@ -16,7 +16,7 @@ _SCRTXT = """
 
 
 🥷 **Scrapped By ->** {}
-👨‍🎤 **Developed By ->** @ll_ALPHA_BABY_lll 🐲
+👨‍🎤 **Developed By ->** @kingxmdb 🐲
 """
 
 
@@ -60,7 +60,7 @@ async def cc_scraper(c, m):
         str(txt.count("\n")),
         m.from_user.mention,
     )
-    file = f"x{limit} CC Scrapped by RAUSHANUB.txt"
+    file = f"x{limit} CC Scrapped by HARSHUB.txt"
     with open(file, "w+") as f:
         f.write(txt)
     del txt
